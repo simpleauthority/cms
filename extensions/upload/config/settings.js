096e0c6bad13ea4ca54ee683ac7649e9a5ac9e54
simpleauthority <jacob@algorithmjunkie.com>
2020-10-15 04:56:44 -0700

    try to use minio instead

--- extensions/upload/config/settings.js
+++ extensions/upload/config/settings.js
@@ -1,8 +1,18 @@
 module.exports = {
-    provider: "backblaze-b2",
-    providerOptions: {
-      accountId: process.env.B2_ACCOUNT_ID,
-      applicationKey: process.env.B2_APP_KEY,
-      bucket: process.env.B2_BUCKET
+//   provider: "backblaze-b2",
+//   providerOptions: {
+//    accountId: process.env.B2_ACCOUNT_ID,
+//      applicationKey: process.env.B2_APP_KEY,
+//      bucket: process.env.B2_BUCKET
+//    }
+  provider: 'aws-s3',
+  providerOptions: {
+    accessKeyId: env('AWS_ACCESS_KEY_ID'),
+    secretAccessKey: env('AWS_ACCESS_SECRET'),
+    region: env('AWS_REGION'),
+    endpoint: env('AWS_ENDPOINT'),
+    params: {
+      Bucket: env('AWS_BUCKET'),
     }
   }
+}
