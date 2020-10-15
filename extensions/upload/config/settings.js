28d5b9a6e1dc82d050d7fea45a1800e7062d2eb0
simpleauthority <jacob@algorithmjunkie.com>
2020-10-15 05:20:12 -0700

    =(

--- extensions/upload/config/settings.js
+++ extensions/upload/config/settings.js
@@ -9,8 +9,9 @@ module.exports = {
   providerOptions: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_ACCESS_SECRET,
-    region: process.env.AWS_REGION,
     endpoint: process.env.AWS_ENDPOINT,
+    s3ForcePathStyle: true,
+    signatureVersion: 'v4',
     params: {
       Bucket: process.env.AWS_BUCKET,
     }
