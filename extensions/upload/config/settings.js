07a25c7864096d87955964f5e55f7db340444fb2
simpleauthority <jacob@algorithmjunkie.com>
2021-01-01 16:01:44 -0800

    update to 3.2.5

--- extensions/upload/config/settings.js
+++ extensions/upload/config/settings.js
@@ -1,10 +1,4 @@
 module.exports = {
-//   provider: "backblaze-b2",
-//   providerOptions: {
-//    accountId: process.env.B2_ACCOUNT_ID,
-//      applicationKey: process.env.B2_APP_KEY,
-//      bucket: process.env.B2_BUCKET
-//    }
   provider: 'aws-s3',
   providerOptions: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
